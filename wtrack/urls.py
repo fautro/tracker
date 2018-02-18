6d84bd8500afba81d632a68f5cec9693f0d3d9af
fautro <dogmeatster@gmail.com>
2018-02-18 16:37:32 +0200

    [wtrack] Fixes urlpatterns

--- wtrack/urls.py
+++ wtrack/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from wtrack import views
 
-urlspatterns = [
+urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
