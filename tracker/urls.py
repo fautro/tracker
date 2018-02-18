e844e27ee0f6626996e244d22922935c98fcc743
fautro <dogmeatster@gmail.com>
2018-02-18 16:07:01 +0200

    [wtrack] Adds url catcher for wtrack/ app

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from wtrack import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('wtrack/', include('wtrack.urls')),
 ]
