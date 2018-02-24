6f37f37719ef02aa88c84d6dd8e19dd5254e6aa7
fautro <dogmeatster@gmail.com>
2018-02-24 14:23:56 +0200

    [wtrack] Adds workaround to serve media content as static content

--- tracker/urls.py
+++ tracker/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from wtrack import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('wtrack/', include('wtrack.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
