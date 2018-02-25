ddf8828dfdc25896bcef39c585907c247e449c2d
fautro <dogmeatster@gmail.com>
2018-02-25 13:24:20 +0200

    [wtrack] Creates a model for weight tracking

--- wtrack/models.py
+++ wtrack/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Weight(models.Model):
+    CLIMBING_FLAGS = (
+        ('B', 'Bouldering'),
+        ('BM', 'Bouldering on Moonboard'),
+        ('L', 'Lead'),
+        ('D', 'Dry')
+    )
+
+    GYM_FLAGS = (
+        ('L', 'Leg Day'),
+        ('LS', 'Leg Day Short'),
+        ('U', 'Upper Body Day'),
+        ('US', 'Upper Body Day Short'),
+        ('W', 'Whole Body Workout'),
+        ('C', 'Cardio')
+    )
+
+    ALCO_FLAGS = (
+        ('Y', 'YES'),
+        ('N', 'NO')
+    )
+
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    day = models.CharField(max_length=3, default=day_of_week(date))
+    morning_weight = models.DecimalField(max_digits=5, decimal_places=2)
+    evening_weight = models.DecimalField(max_digits=5, decimal_places=2)
+    sleep_hours = models.SmallIntegerField()
+    calories_consumed = models.SmallIntegerField()
+    climbing_flag = models.CharField(max_length=2, choices=CLIMBING_FLAGS)
+    gym_flag = models.CharField(max_length=2, choices=GYM_FLAGS)
+    alco_flag = models.CharField(max_length=1, choices=ALCO_FLAGS)
+
+    def __str__(self):
+        return self.date
+
+    def day_of_week(date):
+        day_nr = dt.datetime(date).weekday()
+        day_arr = ['Mo', 'Tue', 'Wed', 'Thu', 'Fr', 'Sat', 'Sun']
+        return day_arr[day_nr]
