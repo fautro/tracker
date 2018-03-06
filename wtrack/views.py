4d0252f0a585042ec64bfc497cd54584bca58d53
fautro <dogmeatster@gmail.com>
2018-03-06 20:47:55 +0200

    [wtrack] Defines fields in view and fixes index

--- wtrack/views.py
+++ wtrack/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from wtrack.models import Record
 
 def index(request):
-    record_list = Record.objects.order_by('-date')[:30]
+    record_list = Record.objects.values('date', 'sleep_hours', 'calories_consumed', \
+                                        'climbing_flag', 'gym_flag', 'alco_flag', \
+                                        'morningweight__morning_weight', \
+                                        'eveningweight__evening_weight').order_by('-date')[:30]
     context_dict = {'records': record_list}
     return render(request, 'wtrack/index.html', context=context_dict)
 
