07d4e9880d1544d2b76718d50257e2571d0cbca0
fautro <dogmeatster@gmail.com>
2018-03-15 21:52:27 +0200

    [wtrack] Big Bang refactoring

--- wtrack/forms.py
+++ wtrack/forms.py
@@ -35,7 +35,7 @@ class AdditionsForm(forms.ModelForm):
     for result in Weight.objects.values('date').order_by('-date')[:30]:
         DATE_QUERYSET.append(str(result['date']))
 
-    date = forms.ModelChoiceField(queryset=DATE_QUERYSET)
+    date = forms.ModelChoiceField(queryset=tuple(DATE_QUERYSET))
     ## day = models.CharField(max_length=3, default=day_of_week(date))
     sleep_hours = forms.IntegerField()
     calories_consumed = forms.IntegerField()
