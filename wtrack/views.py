7473e52c4a0c3ae7cf3a9e50261454c07f8f3c60
fautro <dogmeatster@gmail.com>
2018-10-21 19:22:12 +0300

    [wtrack] tries to pass user and hky

--- wtrack/views.py
+++ wtrack/views.py
@@ -41,6 +41,8 @@ def add_record(request):
         weight_form = WeightForm(request.user, request.POST)
         additions_form = AdditionsForm(request.user, request.POST)
         if 'submit_weight' in request.POST and weight_form.is_valid():
+            HKY = weight_form.cleaned_data['HKY']
+            user = weight_form.cleaned_data['user']
             weight_form.save()
         if 'submit_addition' in request.POST and additions_form.is_valid():
             additions_form.save()
