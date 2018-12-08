9d477d8c1efbd63584fa0b6eb03d84e6bcd75008
fautro <dogmeatster@gmail.com>
2018-12-08 15:06:31 +0200

    [wtrack] fixes additions

--- wtrack/views.py
+++ wtrack/views.py
@@ -43,13 +43,13 @@ def add_record(request):
         additions_form = AdditionsForm(request.user, request.POST)
         if 'submit_weight' in request.POST and weight_form.is_valid():
             weight = weight_form.save(commit=False)
-            weight.user = request.user
+            weight.user =
             concat_date = request.POST['date_year']+request.POST['date_month']+request.POST['date_day']
             weight.HKY = calc_hash(request.user.username, concat_date)
             weight.save()
         if 'submit_addition' in request.POST and additions_form.is_valid():
             addition = additions_form.save(commit=False)
-            addition.user = 'Fautor'
+            addition.user = request.user
             concat_date = request.POST['date_year']+request.POST['date_month']+request.POST['date_day']
             addition.HKY = calc_hash(request.user.username, concat_date)
             addition.save()
