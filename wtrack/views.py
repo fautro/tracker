2294f77970760ca259a45ac528e1b464e8e7db4d
fautro <dogmeatster@gmail.com>
2018-12-08 13:52:59 +0200

    [wtrack] tries to pass HKY

--- wtrack/views.py
+++ wtrack/views.py
@@ -44,7 +44,7 @@ def add_record(request):
         if 'submit_weight' in request.POST and weight_form.is_valid():
             weight = weight_form.save(commit=False)
             weight.user = request.user
-            weight.HKY = '799d239d94be7e2c994ba361912ad58b'
+            weight.HKY = 'asdqweasd'
             weight.save()
         if 'submit_addition' in request.POST and additions_form.is_valid():
             additions_form.save()
