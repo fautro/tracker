cdc5ed646feacb2b22dabf39fd355bf832ffe65e
fautro <dogmeatster@gmail.com>
2018-12-28 16:19:15 +0200

    [wtrack] fixes additions

--- wtrack/forms.py
+++ wtrack/forms.py
@@ -47,7 +47,7 @@ class AdditionsForm(forms.ModelForm):
     HKY = forms.ModelChoiceField(queryset=None,
                                  to_field_name = 'date',
                                  required=True)
-    date = forms.DateField(widget=forms.SelectDateWidget(), initial=date.today())
+    #date = forms.DateField(widget=forms.SelectDateWidget(), initial=date.today())
     evening_weight = forms.DecimalField(max_digits=5, decimal_places=2, required=True)
     sleep_hours = forms.IntegerField()
     calories_consumed = forms.IntegerField()
