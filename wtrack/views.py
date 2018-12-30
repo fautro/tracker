b15ec6578db728c6098fdf577f8493f062a00be1
fautro <dogmeatster@gmail.com>
2018-12-30 16:08:01 +0200

    [wtrack] adds ability to edit additions

--- wtrack/views.py
+++ wtrack/views.py
@@ -41,7 +41,7 @@ def add_record(request):
 
     if request.method == 'POST':
         weight_form = WeightForm(request.user, request.POST)
-        #additions_form = AdditionsForm(request.user, request.POST, instance=additions_instance)
+        additions_form = AdditionsForm(request.user, request.POST)
         if 'submit_weight' in request.POST and weight_form.is_valid():
             weight = weight_form.save(commit=False)
             weight.user = request.user
